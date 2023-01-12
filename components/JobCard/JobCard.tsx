26b3ca06b4cb087b12b53156abf2b2c6ca1b5013
Kevin <kevin.espinosa@uruit.com>
2023-01-12 00:48:16 -0500

    Initial frontend setup with tailwind and nextjs

---
+++ components/JobCard/JobCard.tsx
@@ -0,0 +1,16 @@
+import { Tag } from 'components';
+import React from 'react';
+
+export const JobCard = ({ name, description, tags }: any) => {
+  return (
+    <div className="w-full border-2 border-gray-700 border-solid p-8 rounded-sm">
+      <h2 className="font-bold text-lg">{name}</h2>
+      <p className="mb-5">{description}</p>
+      <div className="flex gap-3">
+        {tags.map((tag: string) => (
+          <Tag title={tag} />
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ components/Tag/Tag.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Tag = ({ title }: any) => (
+  <div className="p-2 rounded-2xl font-bold border-2 border-gray-800 border-solid text-xs">
+    {title}
+  </div>
+);
